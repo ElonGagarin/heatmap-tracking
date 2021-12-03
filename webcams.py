d61efebf6ec36ae4c5db38622338bfba5ea3056c
ElonGagarin <sergeev.ura.v@gmail.com>
2021-12-03 12:28:39 +0300

    1

---
+++ webcams.py
@@ -0,0 +1,23 @@
+# https://www.youtube.com/watch?v=MrHrceiYn2c
+# https://www.youtube.com/watch?v=RQA5RcIZlAM
+# https://www.youtube.com/watch?v=DxgiDvNDjoA
+# https://www.youtube.com/watch?v=1EiC9bvVGnk
+# https://www.youtube.com/watch?v=AdUw5RdyZxI
+# https://www.youtube.com/watch?v=BwcNVdE1CAo
+# https://www.youtube.com/watch?v=BwcNVdE1CAo
+# https://www.youtube.com/watch?v=rLFKJwAo38M
+
+# Проверить yolov5 по центру бб рисовать линию
+# исправить detect.py впихнуть трешхолд по координаде от yolo дозаписывать
+
+# Или сохранять координаты
+# записывать маску
+# получить фото с отсутствием движущихся объекстов
+# наложить маску на чистое фото
+
+
+
+
+# или использовать трекинг
+# тогда раскрашивать линии зависимости от скорости 
+# как считать скорость?
