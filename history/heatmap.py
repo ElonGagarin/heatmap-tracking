df063a17086c077b02dcd13c6d1d2919df66d95a
“ElonGagarin” <sergeev.ura.v@gmail.com>
2021-12-05 07:47:39 +0300

    del

--- history/heatmap.py
+++
@@ -1,7 +0,0 @@
-# https://towardsdatascience.com/automatic-vision-object-tracking-347af1cc8a3b
-
-# https://bipuljain.wordpress.com/2011/12/20/multiple-color-tracking-using-opencv-python/
-
-# https://towardsdatascience.com/build-a-motion-heatmap-videousing-opencv-with-python-fd806e8a2340
-
-# https://pythondigest.ru/view/6908/
